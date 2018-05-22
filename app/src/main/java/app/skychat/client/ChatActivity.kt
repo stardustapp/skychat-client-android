19e84bbfba0d12ff4868b9e09e026578175be8ca
Daniel Lamando <apt@danopia.net>
2018-05-22 02:08:42 -0700

    Implement a few basic slash commands, incl /me

--- app/src/main/java/app/skychat/client/ChatActivity.kt
+++ app/src/main/java/app/skychat/client/ChatActivity.kt
@@ -14,16 +14,19 @@ import android.widget.Toast
 import app.skychat.client.actions.ListRoomsTask
 import app.skychat.client.data.Profile
 import app.skychat.client.skylink.FolderLiteral
+import app.skychat.client.skylink.NetEntry
 import app.skychat.client.skylink.StringLiteral
 import app.skychat.client.skylink.remoteTreeFor
 import com.bugsnag.android.Bugsnag
 import com.google.common.collect.ImmutableMap
+import io.reactivex.Maybe
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.app_bar_chat.*
 import kotlinx.android.synthetic.main.content_chat.*
 import kotlinx.android.synthetic.main.nav_header_chat.*
+import java.util.*
 
 class ChatActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, ActivityFragment.OnListFragmentInteractionListener {
     companion object {
@@ -35,26 +38,68 @@ class ChatActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     private var menuIds: Map<Int, ListRoomsTask.RoomEntry> = emptyMap()
 
+    fun sendIrcPacket(command: String, vararg params: String): Maybe<NetEntry> {
+        return remoteTreeFor(profile.domainName!!)
+                .invokeRx("/sessions/${profile.sessionId}/mnt/runtime/apps/irc/namespace/state/networks/${"freenode"}/wire/send/invoke",
+                        FolderLiteral("",
+                                StringLiteral("command", command),
+                                FolderLiteral("params", *params.mapIndexed({ index, s ->
+                                    StringLiteral((index+1).toString(), s)
+                                }).toTypedArray())))
+                .filter { evt -> evt.type == "String" }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
         setSupportActionBar(toolbar)
 
         send_message_btn.setOnClickListener { view ->
+            val currentTarget = currentRoom?.name ?: "0"
+
             val message = message_input.text.toString()
             //message_input.text = ""
-            remoteTreeFor(profile.domainName!!)
-                    .invokeRx("/sessions/${profile.sessionId}/mnt/runtime/apps/irc/namespace/state/networks/${"freenode"}/wire/send/invoke",
-                            FolderLiteral("",
-                                    StringLiteral("command", "PRIVMSG"),
-                                    FolderLiteral("params",
-                                            StringLiteral("1", currentRoom?.name!!),
-                                            StringLiteral("2", message))))
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe { x ->
-                        Snackbar.make(view, "Message sent $x", Snackbar.LENGTH_SHORT).show()
+
+            val maybe = if (message[0] == '/') {
+                val parts = message.drop(1).split(' ')
+                val command = parts[0].toLowerCase()
+                val params = parts.drop(1)
+
+                when (command) {
+                    "msg" -> sendIrcPacket(
+                            "PRIVMSG", params[0],
+                            params.drop(1).joinToString(" "))
+                    "me" -> sendIrcPacket(
+                            "CTCP", currentTarget,
+                            "ACTION", params.joinToString(" "))
+                    "slap" -> sendIrcPacket(
+                            "CTCP", currentTarget,
+                            "ACTION", "slaps ${params.joinToString(" ")} around a bit with a large trout")
+                    "ctcp" -> sendIrcPacket(
+                            "CTCP", params[0],
+                            params[1], params.drop(2).joinToString(" "))
+                    else -> Maybe.error(
+                            IllegalArgumentException("Invalid slash command /$command"))
+                }
+            } else sendIrcPacket(
+                    "PRIVMSG", currentTarget,
+                    message) // TODO: splitting for length
+
+            maybe
+                .observeOn(AndroidSchedulers.mainThread())
+                .defaultIfEmpty(StringLiteral("output", "Unknown error"))
+                .subscribe({ x ->
+                    if (x.stringValue == "Ok") {
                         message_input.text.clear()
+                    } else {
+                        Snackbar.make(view, "Failed to send message",
+                                Snackbar.LENGTH_LONG).show()
                     }
+                }, { err ->
+                    Bugsnag.notify(err)
+                    Snackbar.make(view, "Crashed sending message: ${err.message}",
+                            Snackbar.LENGTH_LONG).show()
+                })
         }
 
         val profileId = this.intent.getStringExtra(EXTRA_PROFILE)
